3f5fe61ebf5bea4b34406b48e2a3d080c9227111
PedroSampaioDev <pedrosampaio11@icloud.com>
2025-01-24 15:43:01 -0300

    feat: add components in navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { LogoBox, NavbarContainer } from "./style";
+import { LogoBox, NavbarContainer, NavLink, NavBtn } from "./style";
 
 export const Navbar: React.FC = () => {
   return (
     <NavbarContainer>
       <LogoBox>Logo</LogoBox>
+      <NavLink>Links</NavLink>
+      <NavBtn>Button</NavBtn>
     </NavbarContainer>
   );
 };
--- src/components/Navbar/style.tsx
+++ src/components/Navbar/style.tsx
@@ -3,7 +3,15 @@ import styled from "styled-components";
 export const NavbarContainer = styled.div({
   padding: "1.5rem 2rem",
   display: "flex",
+  justifyContent: "space-between",
   color: "#fff"
 });
 
+export const NavLink = styled.div`
+  
+`;
+
+export const NavBtn = styled.div`
+  
+`;
 export const LogoBox = styled.div({});
