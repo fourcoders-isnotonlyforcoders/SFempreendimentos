cb634ed09624d5d83919393d9a1589163af9f3af
Ruan Dias <ruan.dias77@outlook.com>
2025-02-28 18:05:55 -0300

    feat: corrigin bug de carrossel retornando

--- src/components/SecaoCarrousel/Carrousel.tsx
+++ src/components/SecaoCarrousel/Carrousel.tsx
@@ -3,41 +3,40 @@ import "keen-slider/keen-slider.min.css";
 import {
   CarrouselContainer,
   CarrouselImageContainer,
-  CarrouselCardImage,
-} from "./styles"; 
+  CarrouselCardImage
+} from "./styles";
+import { PORTIFOLIO } from "../../constants/portifolio";
 
-const animation = { duration: 4000, easing: (t: number) => t };
+const animation = { duration: 40000, easing: (t: number) => t };
 
 export const Carrousel: React.FC = () => {
+  const numberOfCards = PORTIFOLIO.length;
   const [sliderRef] = useKeenSlider({
     loop: true,
-    renderMode: "performance", 
-    drag: true, 
+    renderMode: "performance",
+    drag: true,
     slides: { perView: 3, spacing: 20 },
     created(s) {
-      s.moveToIdx(1, true, animation); 
+      s.moveToIdx(numberOfCards - 1, true, animation);
     },
     updated(s) {
-      s.moveToIdx(s.track.details.abs + 1, true, animation); 
+      s.moveToIdx(s.track.details.abs + numberOfCards - 1, true, animation);
     },
     animationEnded(s) {
-      s.moveToIdx(s.track.details.abs + 1, true, animation); 
-    },
+      s.moveToIdx(s.track.details.abs + numberOfCards - 1, true, animation);
+    }
   });
 
   return (
     <CarrouselContainer>
       <CarrouselImageContainer ref={sliderRef} className="keen-slider">
-        <CarrouselCardImage className="keen-slider__slide" src="/images/project1.jpg" />
-        <CarrouselCardImage className="keen-slider__slide" src="/images/project2.jpg" />
-        <CarrouselCardImage className="keen-slider__slide" src="/images/project3.jpg" />
-        <CarrouselCardImage className="keen-slider__slide" src="/images/project4.jpg" />
-        <CarrouselCardImage className="keen-slider__slide" src="/images/project5.jpg" />
-        <CarrouselCardImage className="keen-slider__slide" src="/images/project6.jpg" />
-        <CarrouselCardImage className="keen-slider__slide" src="/images/project7.jpg" />
-        <CarrouselCardImage className="keen-slider__slide" src="/images/project8.jpg" />
-        <CarrouselCardImage className="keen-slider__slide" src="/images/project9.jpg" />
-        <CarrouselCardImage className="keen-slider__slide" src="/images/project10.jpg" />
+        {PORTIFOLIO.map(({ src }, index) => (
+          <CarrouselCardImage
+            key={index}
+            className="keen-slider__slide"
+            src={src}
+          />
+        ))}
       </CarrouselImageContainer>
     </CarrouselContainer>
   );
